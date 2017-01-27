9a1d700b9c2b9b5e3c7a9f26e48ef254ca47212c
StephanK9 <stephane.khoury@gmail.com>
2017-01-27 09:03:23 -0800

    create a service for our member, and injecting the service into the HQ component

--- src/app/headquarter/headquarter.component.ts
+++ src/app/headquarter/headquarter.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-headquarter',
   templateUrl: './headquarter.component.html',
-  styleUrls: ['./headquarter.component.css']
+  styleUrls: ['./headquarter.component.css'],
+  providers: [MemberService]
 })
-export class HeadquarterComponent {
+export class HeadquarterComponent implements OnInit {
+  members: Member[];
+  
+  constructor(private router: Router, private memberService: MemberService){}
 
-  constructor(private router: Router){}
+  ngOnInit(){
+    this.members = this.memberService.getMembers();
+  }
 
-  members: Member[] = [
-    new Member("Alphonso", "Online", "Let me know if you want to go to the movies on Sunday!", 1),
-    new Member("Ralph", "Offline", "Going bowling tomorrow, lmk", 2),
-    new Member("Julia", "Online", "Shopping spree Friday??", 3),
-    new Member("Lucy", "Online", "Let's go to the cabin in March?", 4)
-  ];
+
+
 
   goToDetailPage(clickedMember: Member) {
     this.router.navigate(['members', clickedMember.id]);
---
+++ src/app/mock-members.ts
@@ -0,0 +1,8 @@
+import { Member } from './member.model';
+
+export const MEMBERS: Member[] = [
+  new Member("Alphonso", "Online", "Let me know if you want to go to the movies on Sunday!", 1),
+  new Member("Ralph", "Offline", "Going bowling tomorrow, lmk", 2),
+  new Member("Julia", "Online", "Shopping spree Friday??", 3),
+  new Member("Lucy", "Online", "Let's go to the cabin in March?", 4)
+];

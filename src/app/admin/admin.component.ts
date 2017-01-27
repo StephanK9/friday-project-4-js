ed06484fcde21921f33d499dbff94ff26058b82b
StephanK9 <stephane.khoury@gmail.com>
2017-01-27 09:46:46 -0800

    add admin route, form to add new members to the group, saving to firebase

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { MemberService } from '../member.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+  providers: [MemberService]
+})
+export class AdminComponent implements OnInit {
+
+  constructor(private memberService: MemberService) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(name: string, status: string, description: string) {
+    var newMember: Member = new Member(name, status, description);
+    this.memberService.addMember(newMember);
+  }
+
+}
